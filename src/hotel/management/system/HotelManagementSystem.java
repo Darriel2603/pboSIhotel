1a9ed396f52cead9404a088f47a6f8c1f4f14e0b
Darriel2603 <darrielmarkerizal@gmail.com>
2023-05-26 16:52:06 +0700

    first commit

---
+++ src/hotel/management/system/HotelManagementSystem.java
@@ -0,0 +1,7 @@
+package hotel.management.system;
+
+public class HotelManagementSystem {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/hotel/management/system/Login.java
@@ -0,0 +1,47 @@
+package hotel.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Login extends JFrame {
+    Login() {
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+
+        JLabel user = new JLabel("Username");
+        user.setBounds(150, 50, 100, 30);
+        add(user);
+
+        JTextField username = new JTextField();
+        username.setBounds(230, 50, 150, 30);
+        add(username);
+
+        JLabel pass = new JLabel("Password");
+        pass.setBounds(150, 100, 100, 30);
+        add(pass);
+
+        JTextField password = new JTextField();
+        password.setBounds(230, 100, 150, 30);
+        add(password);
+
+        JButton login = new JButton("Login");
+        login.setBounds(140, 180, 120, 30);
+        login.setBackground(Color.BLACK);
+        login.setForeground(Color.WHITE);
+        add(login);
+
+        JButton cancel = new JButton("Cancel");
+        cancel.setBounds(280, 180, 120, 30);
+        cancel.setBackground(Color.BLACK);
+        cancel.setForeground(Color.WHITE);
+        add(cancel);
+
+        setBounds(500, 200, 600, 300);
+        setResizable(false);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Login();
+    }
+}
